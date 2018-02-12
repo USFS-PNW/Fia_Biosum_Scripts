0cdd11bc19456ccb4bc5ecfcc4be2000a07153d1
carlinstarrs <carlinstarrs@berkeley.edu>
2018-02-12 10:48:29 -0800

    Added plot_near.txt to additional data for the yard_dist script and updated yard_dist with more comments
    yard_dist is correct but needs the plot_near.txt file to run.

--- R scripts/yard_dist.R
+++ R scripts/yard_dist.R
@@ -5,9 +5,13 @@
 #imported into the master.mdb file and used to update the gis_yard_dist
 #value in the plot table by linking plot (NOT biosum_plot_id), measyear, 
 #invyr, statecd, and fvs_variant. 
+library("dplyr")
+library("RODBC")
+options(scipen = 999)
 
+setwd("H:/cec_20170915/gis/")
 
-setwd("D:/cec_20170915/gis/")
+#plot_near.txt is in the additional data on github
 plot_near <- read.csv("plot_near.txt", colClasses = c(rep("numeric",3), "character", rep("numeric", 9), rep("character", 8), "numeric", "character", rep("numeric", 17)))
 yard_dist_2017 <- plot_near[,c(4:6,9:10,14, 17:21,38)]
 yard_dist_2017$gis_yard_d <- yard_dist_2017$NEAR_DIST * 3.28084
@@ -18,3 +22,6 @@ names(yard_dist_2017)[1] <- "biosum_plot_id"
 yard_dist_2017$NEAR_DIST <- NULL
 
 write.csv(yard_dist_2017, "yard_dist_2017.csv")
+
+#then merge this in to the master/plot table to update gis_yard_dist based on the plot_id/plot values (biosum_plot_id gets
+#screwed up and isn't correct)
