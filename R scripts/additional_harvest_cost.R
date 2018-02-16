eb1e74319d6ab63a692c6d9101d0ae3cc242da35
carlinstarrs <carlinstarrs@berkeley.edu>
2018-02-16 13:00:43 -0800

    update additionl harvest costs script
    Updated this script to add CTL lop and scatter values to match CEC report (though there were no CTL lop and scatter prescriptions for the project).
    
    The script was also updated to back up any tables it replaces, and instead of replacing with a new table, it uses a join/update query to update values in the tables. This was in order to hopefully avoid issues with the primary key values not remaining in the scenario_additional_harvest_costs table when a table is added via R, which may have caused core analysis to hang during audit.  The primary key values should now remain.

--- R scripts/additional_harvest_cost.R
+++ R scripts/additional_harvest_cost.R
@@ -7,8 +7,6 @@ options(scipen = 999) #this is important for making sure your stand IDs do not g
 #This takes the rx package information, translates it, and prepares it for 
 #updating the scenario_additional_harvest_costs table. 
 
-setwd("H:/cec_20170915/db/") #update working directory to your own location
-
 conn <- odbcDriverConnect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=H:/cec_20170915/db/fvsmaster.mdb") #update text after "DBQ=" to the location of your fvsmaster.mdb file
 fvsmaster_PkgLabels <- sqlFetch(conn, "PkgLabels", as.is = TRUE) #This PkgLabels table was added separately from the packagelabels.csv file (see github additional data)
 fvsmaster_rx_pkg_xwalk <- sqlFetch(conn, "rx_pkg_xwalk", as.is = TRUE)
@@ -37,7 +35,8 @@ rx_addcost_update$ColumnName[rx_addcost_update$rx == 999] <- "None"
 
 rx_addcost_update$Description <- paste("Cost of", rx_addcost_update$ColumnName, sep = " ")
 
-#removes the old rx_harvest_cost_columns table; make sure you have this backed up if you need it later
+#backs up the old rx_harvest_cost_columns table and updates with new
+sqlQuery(conn, 'SELECT * INTO rx_harvest_cost_columns_old FROM rx_harvest_cost_columns')
 sqlQuery(conn, 'DROP TABLE rx_harvest_cost_columns') 
 
 sqlSave(conn, dat = rx_addcost_update, tablename = "rx_harvest_cost_columns", rownames = FALSE)
@@ -49,7 +48,7 @@ master_cond <- sqlFetch(conn, "cond", as.is = TRUE)
 odbcCloseAll()
 
 #Make sure you have opened the processor module with this project to make sure this table is populated
-conn <- odbcDriverConnect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=H:/cec_20170915/processor/db/scenario_processor_rule_definitions.mdb") #update text after "DBQ=" to the location of your scenario_processor_rule_definitions.mdb file
+conn <- odbcDriverConnect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=G:/Dropbox/Carlin/Berkeley/biosum/scenario_processor_rule_definitions.mdb") #update text after "DBQ=" to the location of your scenario_processor_rule_definitions.mdb file
 scenario_additional_harvest_costs <- sqlFetch(conn, "scenario_additional_harvest_costs", as.is = TRUE)
 
 slope <- master_cond[,c(1,16)] #get slope values for each condition/stand from master_cond
@@ -63,20 +62,19 @@ scenario_update <- merge(scenario_update, rx_addcost2, by = "rx") #add in additi
 scenario_update$RX_burn[scenario_update$ColumnName == "RX_burn" & scenario_update$slope <= 40] <- 75
 scenario_update$RX_burn[scenario_update$ColumnName == "RX_burn" & scenario_update$slope > 40] <- 150
 scenario_update$Masticate[scenario_update$ColumnName == "Masticate" & scenario_update$slope <= 40] <- 250
-scenario_update$Lop_and_scatter[scenario_update$ColumnName == "Lop_and_scatter" & scenario_update$slope <= 40] <- 35
-scenario_update$Lop_and_scatter[scenario_update$ColumnName == "Lop_and_scatter" & scenario_update$slope > 40] <- 60
+scenario_update$Lop_and_scatter[scenario_update$ColumnName == "Lop_and_scatter" & scenario_update$slope <= 40 & scenario_update$Type == "WT"] <- 35
+scenario_update$Lop_and_scatter[scenario_update$ColumnName == "Lop_and_scatter" & scenario_update$slope > 40 & scenario_update$Type == "WT"] <- 60
+scenario_update$Lop_and_scatter[scenario_update$ColumnName == "Lop_and_scatter" & scenario_update$slope <= 40 & scenario_update$Type == "CTL"] <- 50
+scenario_update$Lop_and_scatter[scenario_update$ColumnName == "Lop_and_scatter" & scenario_update$slope > 40 & scenario_update$Type == "CTL"] <- 75
 scenario_update$Pile_burn[scenario_update$ColumnName == "Pile_burn" & scenario_update$slope <= 40 & scenario_update$Type == "WT"] <- 125
 scenario_update$Pile_burn[scenario_update$ColumnName == "Pile_burn" & scenario_update$slope <= 40 & scenario_update$Type == "CTL"] <- 250
 
+sqlQuery(conn, 'SELECT * INTO addl_harvest_cost_update_old FROM addl_harvest_cost_update')
 sqlQuery(conn, 'DROP TABLE addl_harvest_cost_update') #This removes the old addl_harvest_cost_update table; make sure you have it backed up if needed
 
 sqlSave(conn, dat = scenario_update, tablename = "addl_harvest_cost_update", rownames = FALSE)
 
-scenario_additional_harvest_costs <- scenario_update[,c(3,2,1,4,5,6,7,8)] #updates scenario_additional_harvest_costs with new data
-
-sqlQuery(conn, 'DROP TABLE scenario_additional_harvest_costs') #This removes the old scenario_additional_harvest_costs table; make sure you have it backed up if needed
-
-sqlSave(conn, dat = scenario_additional_harvest_costs, tablename = "scenario_additional_harvest_costs", rownames = FALSE)
+sqlQuery(conn, 'SELECT * INTO scenario_additional_harvest_costs_old FROM scenario_additional_harvest_costs')
 
 sqlQuery(conn, 'UPDATE addl_harvest_cost_update INNER JOIN scenario_additional_harvest_costs ON (addl_harvest_cost_update.scenario_id = scenario_additional_harvest_costs.scenario_id) AND (addl_harvest_cost_update.biosum_cond_id = scenario_additional_harvest_costs.biosum_cond_id) AND (addl_harvest_cost_update.rx = scenario_additional_harvest_costs.rx) SET scenario_additional_harvest_costs.RX_burn = [addl_harvest_cost_update].[RX_burn];')
 sqlQuery(conn, 'UPDATE addl_harvest_cost_update INNER JOIN scenario_additional_harvest_costs ON (addl_harvest_cost_update.scenario_id = scenario_additional_harvest_costs.scenario_id) AND (addl_harvest_cost_update.biosum_cond_id = scenario_additional_harvest_costs.biosum_cond_id) AND (addl_harvest_cost_update.rx = scenario_additional_harvest_costs.rx) SET scenario_additional_harvest_costs.Pile_burn = [addl_harvest_cost_update].[Pile_burn];')
